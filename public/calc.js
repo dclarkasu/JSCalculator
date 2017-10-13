15813cd027a39099922ba4ba9dcb386992095b08
dclarkasu <dclark205@gmail.com>
2017-10-13 16:51:39 -0600

    Display numbers clicked

--- public/calc.js
+++ public/calc.js
@@ -3,19 +3,14 @@ $(document).ready(function(){
 });
 
 $('#clear').click(function(e){
-  console.log("CLEAR CLICKED");
+  // console.log("CLEAR CLICKED");
     $('#display').text('');
 });
 
-$(num).click(function(e){
+$('.number').click(function(e){
   var display = $('#display').text();
+  $('#display').text(display + $(this).text());
 });
 
-switch ($('.number').text()) {
-  case '1':
-    console.log("Num 1");
-    break;
-  default:
 
-}
 //use a switch to check the current click value
