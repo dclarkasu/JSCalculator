716a9dfc1d442331feaa7f97fcf56f96ea5f032c
dclarkasu <dclark205@gmail.com>
2017-10-13 17:56:47 -0600

    create global variables and start operator event

--- public/calc.js
+++ public/calc.js
@@ -1,16 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function() {
   console.log('Loaded');
 });
+//global vars to hold numbers entered
+var operand1;
+var operand2;
+var tempValue = [];
+var op = null;
 
-$('#clear').click(function(e){
+$('#clear').click(function(e) {
   // console.log("CLEAR CLICKED");
     $('#display').text('');
 });
 
-$('.number').click(function(e){
+$('.number').click(function(e) {
   var display = $('#display').text();
+  tempValue.push(display);
+  var result = tempValue.join('');
   $('#display').text(display + $(this).text());
+  console.log(result);
+});
+
+$('.operator').click(function(e) {
+  console.log("Btn clicked");
+  // switch (expression) {
+  //   case expression:
+  //
+  //     break;
+  //   default:
+  //
+  // }
 });
 
 
-//use a switch to check the current click value
+// Press operator:
+// perform a join
+// operand1 = result (the joined string)
+// save the selected operator into op
+//
+// press = :
+// set op2 = result (the joined string)
+// Then perform actual math operation and store/display
