e0648b63e8f7bffcbb4e3da677fda4001c60c899
dclarkasu <dclark205@gmail.com>
2017-10-14 12:39:23 -0600

    Addition and equals function

--- public/calc.js
+++ public/calc.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   console.log('Loaded');
 });
 //global vars to hold numbers entered
-var operand1;
-var operand2;
+var operand1 = '';
+var operand2 = '';
 var tempValue = [];
 var op = null;
 
@@ -18,20 +18,37 @@ $('#clear').click(function(e) {
 
 $('.number').click(function(e) {
   var display = $('#display').text();
-  tempValue.push(display);
-  var result = tempValue.join('');
   $('#display').text(display + $(this).text());
-  console.log(result);
+  tempValue.push($(this).text());
+  console.log(tempValue)
+  // console.log(result);
 });
 
 $('.operator').click(function(e) {
-  op = $(this).text();
-
+  var oldOp = '';
+  if($(this).text() === '='){
+    oldOp = op;
+    op = $(this).text();
+  }
+  else{
+    op = $(this).text();
+  }
   switch (op) {
     case '+':
-    var result = tempValue.join('');
-      operand1 = result;
-      console.log(result);
+    if(operand1 === '') {
+      operand1 = tempValue.join('');
+      $('#display').text('')
+      tempValue = [];
+      op = '+';
+    }
+    else if(operand2 === '') {
+      operand2 = tempValue.join('');
+      $('#display').text('')
+      tempValue = [];
+      op = '+';
+    }
+      console.log(operand1 + '' + op);//???
+      console.log(operand2 + '' + op);//???
       break;
     case '-':
 
@@ -42,8 +59,20 @@ $('.operator').click(function(e) {
     case '/':
 
       break;
-    case '=':
+    case '='://?? How do I perform the right action on equals
 
+    if(operand2 === '') {
+      operand2 = tempValue.join('');
+      $('#display').text('')
+      tempValue = [];
+    }
+    console.log('operand1 ' + operand1);
+    console.log('operand2 ' + operand2);
+      if(oldOp === '+') {
+        var result = parseInt(operand1) + parseInt(operand2);
+        console.log(result);
+        $('#display').text(result)
+      }
       break;
     default:
 
@@ -51,9 +80,9 @@ $('.operator').click(function(e) {
 });
 
 
+
 // Press operator:
-// perform a join
-// operand1 = result (the joined string)
+// perform a join and store it
 // save the selected operator into op
 //
 // press = :
